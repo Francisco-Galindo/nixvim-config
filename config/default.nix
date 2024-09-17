92e999617632c2d52e2e482fb3e26251615ca03a
Francisco-Galindo <paqui10718@gmail.com>
2024-09-17 00:30:57 -0600

    Adding img-paste.vim

--- config/default.nix
+++ config/default.nix
@@ -52,13 +52,25 @@
     emmet.enable = true;
   };
 
-  extraPlugins = [(pkgs.vimUtils.buildVimPlugin {
-    name = "flexoki-neovim";
-    src = pkgs.fetchFromGitHub {
-      owner = "Francisco-Galindo";
-      repo = "flexoki-neovim";
-      rev = "0757646cb2e646e715d82e10df47657c9ef03408";
-      hash = "sha256-VAasFvYM7emw26nrI/G5Aktcqfi29FEeH0d1PCucNz4=";
-    };
-  })];
+  extraPlugins = [
+    (pkgs.vimUtils.buildVimPlugin {
+      name = "flexoki-neovim";
+      src = pkgs.fetchFromGitHub {
+        owner = "Francisco-Galindo";
+        repo = "flexoki-neovim";
+        rev = "0757646cb2e646e715d82e10df47657c9ef03408";
+        hash = "sha256-VAasFvYM7emw26nrI/G5Aktcqfi29FEeH0d1PCucNz4=";
+      };
+    })
+    (pkgs.vimUtils.buildVimPlugin {
+      name = "img-paste";
+      src = pkgs.fetchFromGitHub {
+        owner = "img-paste-devs";
+        repo = "img-paste.vim";
+        rev = "787be15ff52ea2db5ae7b813ab8808c8d4cf749e";
+        hash = "sha256-LaqEGKbpZmcOJeIz00Ts5DvbFnduScBEUnisTcX1srI=";
+      };
+    })
+  ];
+
 }
