ceb31982709ec8c0fc7806b8a34b3c01c9399df6
Francisco-Galindo <paqui10718@gmail.com>
2024-10-17 12:43:18 -0600

    Use base16 colorscheme instead of separate plugin

---
+++ config/base16.nix
@@ -0,0 +1,24 @@
+{
+  colorschemes.base16 = {
+    enable = true;
+    colorscheme = {
+      base00 = "#100f0f";
+      base01 = "#1c1b1a";
+      base02 = "#282726";
+      base03 = "#343331";
+      base04 = "#403e3c";
+      base05 = "#cecdc3";
+      base06 = "#878580";
+      base07 = "#cecdc3";
+      base08 = "#af3029";
+      base09 = "#da702c";
+      base0A = "#d0a215";
+      base0B = "#66800b";
+      base0C = "#3aa99f";
+      base0D = "#4385be";
+      base0E = "#5e409d";
+      base0F = "#a02f6f";
+    };
+  };
+}
+
--- config/default.nix
+++ config/default.nix
@@ -5,10 +5,10 @@
     ./lsp.nix
     ./undotree.nix
     ./ftplugin/default.nix
+    ./base16.nix
   ];
 
   globals.mapleader = " ";
-  colorscheme = "flexoki-dark";
 
   opts = {
     updatetime = 200;
@@ -85,15 +85,6 @@
 
   extraPlugins = [
     pkgs.vimPlugins.vim-svelte
-    (pkgs.vimUtils.buildVimPlugin {
-      name = "flexoki-neovim";
-      src = pkgs.fetchFromGitHub {
-        owner = "Francisco-Galindo";
-        repo = "flexoki-neovim";
-        rev = "0757646cb2e646e715d82e10df47657c9ef03408";
-        hash = "sha256-VAasFvYM7emw26nrI/G5Aktcqfi29FEeH0d1PCucNz4=";
-      };
-    })
     (pkgs.vimUtils.buildVimPlugin {
       name = "img-paste";
       src = pkgs.fetchFromGitHub {
