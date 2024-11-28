af331e410fad99ec1c60e5ae2ccc22ab72419dd7
Francisco-Galindo <yo@franciscogalindo.com>
2024-11-27 18:28:02 -0600

    Moving opts to its own file

--- config/default.nix
+++ config/default.nix
@@ -2,6 +2,7 @@
 {
   # Import all your configuration modules here
   imports = [
+    ./opts.nix
     ./lsp.nix
     ./undotree.nix
     ./ftplugin/default.nix
@@ -10,30 +11,6 @@
   ];
 
   globals.mapleader = " ";
-
-  opts = {
-    updatetime = 200;
-    encoding = "utf-8";
-    fileformat = "unix";
-    mouse = "a";
-
-    number = true;
-    relativenumber = true;
-
-    autoindent = true;
-    smartindent = true;
-
-    signcolumn = "yes";
-    colorcolumn = "80";
-
-    ignorecase = true;
-    incsearch = true;
-    smartcase = true;
-    wildmode = "list:longest";
-    wildignorecase = true;
-
-    termguicolors = true;
-  };
 
   plugins = {
     cmp = {
---
+++ config/opts.nix
@@ -0,0 +1,26 @@
+{
+  opts = {
+    updatetime = 200;
+    encoding = "utf-8";
+    fileformat = "unix";
+    mouse = "a";
+
+    number = true;
+    relativenumber = true;
+
+    autoindent = true;
+    smartindent = true;
+
+    signcolumn = "yes";
+    colorcolumn = "80";
+
+    ignorecase = true;
+    incsearch = true;
+    smartcase = true;
+    wildmode = "list:longest";
+    wildignorecase = true;
+
+    termguicolors = true;
+  };
+
+}
